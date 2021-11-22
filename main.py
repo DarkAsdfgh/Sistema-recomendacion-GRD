7f64754648ece9d00a05c63c0b3097462b0c8493
Arcadio Abraham Herrera Abril <arcadio.herrera-abril@stud.uni-due.de>
2021-11-22 13:24:11 +0100

    titulo random

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pandas as pd
+import numpy as np
+import random
+
 
 ratings = pd.read_csv("ratings.csv")
-for col in ratings
-	print(col, ratings[col])
+movies = pd.read_csv("movies.csv")
+
+movies = movies[['movieId', 'title']]
+list_titulos = movies['title'].tolist()
+print(random.sample(list_titulos, 20))
+class Movie:
+    def __init__(self, movieId, title):
+        self.movieId = movieId
+        self.title = title
+
+class Rating:
+    def __init__(self, userId, movieId, rating):
+        self.userId = userId
+        self.movieId = movieId
+        self.rating = rating
