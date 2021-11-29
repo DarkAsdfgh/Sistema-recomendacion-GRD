a69946f62437c3d71c0123af1d89441acb544053
Martin Jose Garcia Muñoz <martin.garcia-munoz@stud.uni-due.de>
2021-11-29 13:07:59 +0100

    user ratings

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import pandas as pd
 import numpy as np
 import random
 
-
-ratings = pd.read_csv("ratings.csv")
-movies = pd.read_csv("movies.csv")
-
-movies = movies[['movieId', 'title']]
-list_titulos = movies['title'].tolist()
-print(random.sample(list_titulos, 20))
 class Movie:
     def __init__(self, movieId, title):
         self.movieId = movieId
@@ -19,3 +12,37 @@ class Rating:
         self.userId = userId
         self.movieId = movieId
         self.rating = rating
+
+    def __str__(self):
+        return "Usuario: " + str(self.userId) + "\n" + "ID: " + str(self.movieId) + "\n" + "Rating: " + str(self.rating) + "\n" 
+
+ratings = pd.read_csv("ratings.csv")
+movies = pd.read_csv("movies.csv")
+
+movies = movies[['movieId', 'title']]
+list_titulos = movies['title'].tolist()
+list_moviesID = movies['movieId'].tolist()
+
+movies = {}
+
+for t, m in zip(list_titulos, list_moviesID):
+    movies[m] = t
+
+randomMovies = random.sample(movies.items(), 4)
+
+valoraciones = []
+
+userID = ratings['userId'].tolist()
+userID = userID[-1] + 1
+
+for movie in randomMovies:
+    valoracion = input("Introduce valoriación de la pelicula " + movie[1] + "\n")
+    rating = Rating(userID,movie[0],valoracion) 
+    valoraciones.append(rating)
+
+
+for v in valoraciones:
+    print(v)
+
+
+
